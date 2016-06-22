01ce703576a94eb8f951823d91cb3d395e30283d
Mark Aberdeen <maberdeen@thinkingphones.com>
2016-06-21 22:12:46 -0400

    Added file output with limit

--- src/main/scala/flink-fail.scala
+++ src/main/scala/flink-fail.scala
@@ -3,30 +3,51 @@ import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer08}
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema
 
+import java.io._
 import java.util.Properties
 
 
 object TestConsumer extends App {
 
-  def whatmessage(incoming: String) : String = {
-    print(s"-> ${incoming}\n")
+  def whatmessage(dumper: PrintWriter, incoming: String) : String = {
+    msgCounter += 1
+    if ((msgCounter % 100) == 0) {
+      print(s"[${msgCounter}]")
+    }
+    if (msgCounter > messageLimit)
+      throw new Exception
+    // print(s"-> ${incoming}\n")
+    dumper.write(s"${incoming}\n")
     incoming
   }
+
+  var msgCounter = 0
+  val messageLimit = 1000
 
   val topicName: String = "portal_entity_audit"
 //  var kafkaCfg = new Kafka("localhost:9092", "localhost:2181", "qa-pat-scala", "earliest")
   val kafkaCfg = new Kafka("qa-kafka1:9092", "qa-zk1:2181", "qa-pat-scala", "latest")
   val props = kafkaCfg.asProperties("myClient")
 
+  val fileout: String = "pat-raw.json"
+
   val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+  print("...opening file writer")
+  val filewriter = new PrintWriter(new File(fileout))
 
   val stream = env.addSource(
     new FlinkKafkaConsumer08[String](
       topicName, new SimpleStringSchema(), props)
     ).name("TestConsumerStream").map { rec =>
-      whatmessage(rec)
+      whatmessage(filewriter, rec)
     }
-  env.execute("TestConsumer")
+  try {
+    env.execute("TestConsumer")
+  } finally {
+    print(s"Closing file writer [${msgCounter} lines]")
+    filewriter.close
+  }
 }
 
 
